ec360bd73e9181211cbd1e75d76d0491aaf18216
Rahman2782 <rahman2782@gmail.com>
2025-09-01 00:57:47 -0400

    added scrolling, centered placholder albums, and added comments to note later

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,9 @@
 import { Library, Search, Heart, DiscAlbum } from "lucide-react"
 
 const Sidebar: React.FC = () => {
-  return (
-    <div className="text-gray-400 h-screen w-72 p-5 text-md bg-blue-500">
-        <div className="space-y-4"> {/* child div */}
+  return ( //blue is a placholder color for the sidebar so i can visualize it better
+    <div className="text-gray-400 h-screen w-50 p-5 text-md bg-blue-800 overflow-y-auto">
+        <div className="space-y-4 "> {/* flex flex-col items-center */}
             <button className="flex items-center space-x-2 hover:text-white cursor-pointer">
                 <Search className="w-6 h-6"/>
                 <p>Search</p>
@@ -19,7 +19,7 @@ const Sidebar: React.FC = () => {
             <hr className="mt-4"/>
 
             {/* Playlists/Saved Albums go here - THESE ARE PLACEHOLDERS*/}
-            <DiscAlbum className="h-14 w-14"/>
+            <DiscAlbum className=" h-14 w-14"/>
             <DiscAlbum className="h-14 w-14"/>
             <DiscAlbum className="h-14 w-14"/>
             <DiscAlbum className="h-14 w-14"/>
