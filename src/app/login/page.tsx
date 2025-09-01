112144905370b8cdd2f7af9b5db557851b5828ed
Rahman2782 <rahman2782@gmail.com>
2025-09-01 04:17:04 -0400

    added button for spotify login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Image from 'next/image';
+import spotifyIcon from '/spotifyIcon.svg'
 
 function page() {
   return (
-    <div className='bg-[#131419] h-screen'>
-      <h1 className='text-white'>Login here</h1>
+    <div className='flex flex-col items-center justify-center bg-[#131419] h-screen'>
+        <button className='rounded-full text-white bg-green-600 hover:bg-green-500 cursor-pointer'>
+            <p className='text-white text-bold text-md py-3 px-6'>Login with Spotify</p>
+        </button>
     </div>
   )
 }
